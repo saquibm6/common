3509963d6c47c223cb90ba096684678618c50eb8
saquibm6 <43640360+saquibm6@users.noreply.github.com>
2023-05-25 09:50:55 +0530

    Update azure_redis.tf
    go through the comments

--- azure_redis.tf
+++ azure_redis.tf
@@ -1,4 +1,4 @@
-provider "azurerm" {
+/*provider "azurerm" {
     subscription_id = var.subscription_id
     client_id = var.client_id
   }
@@ -7,19 +7,20 @@ provider "azurerm" {
     name = "ai-dev"
     location = "eastus"
   }
-
+Not needed, is already defined*/
   resource "azurerm_redis_cache" "word_cache_redis" {
-    name = "word-cache-memoryDB"
+    name = "${local.prefix}-${var.ccx_ai_svc_name}-redis"
     resource_group_name = azurerm_resource_group.redis.name
     location = azurerm_resource_group.redis.location
-    family = "P"
-    sku_name = "Premium"
+    family = "C"
+    sku_name = "Standard"
     capacity = 1
     enable_non_ssl_port = true
     public_network_access_enabled = false
     redis_version = 6
-    shard_count = 3
+    #shard_count = 3 Not set for Standard tier
 
+      /* The default policy on this has better policy than the one mentioned here.
     redis_configuration {
       maxmemory_policy = "LRU"
     }
@@ -27,22 +28,45 @@ provider "azurerm" {
       primary = "my-primary-key"
       secondary = "my-secondary-key"
     }
+     The "access_keys" is not a valid block for resource of type azurerm_redis_cache. See https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/redis_cache for more details.
+     This is an output and not an input */
   }
+         
+  resource "azurerm_private_endpoint" "redis" {
+  name                = "${azurerm_redis_cache.word_cache_redis.name}-endpoint"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  subnet_id           = azurerm_subnet.private_endpoints.id
+
+  private_service_connection {
+    name                           = "${azurerm_redis_cache.word_cache_redis.name}-privateconnection"
+    is_manual_connection           = false
+    subresource_names              = ["redis"]
+    private_connection_resource_id = azurerm_redis_cache.word_cache_redis.id
+  }
+  
+  tags = local.tags
+  lifecycle {
+    ignore_changes = [
+      private_dns_zone_group
+    ]
+  }
+}
 
   output "redis_hostname" {
-    value = azurerm_redis_cache.redis.hostname
+    value = azurerm_redis_cache.word_cache_redis.hostname
   }
 
   output "redis_port" {
-    value = azurerm_redis_cache.redis.port
+    value = azurerm_redis_cache.word_cache_redis.port
   }
 
   output "redis_primary_key" {
-    value = azurerm_redis_cache.redis.access_keys.primary
+    value = azurerm_redis_cache.word_cache_redis.access_keys.primary
   }
 
   output "redis_secondary_key" {
-    value = azurerm_redis_cache.redis.access_keys.secondary
+    value = azurerm_redis_cache.word_cache_redis.access_keys.secondary
   }
 }
     
